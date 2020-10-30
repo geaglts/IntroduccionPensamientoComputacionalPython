e0c4c2df5f4d5983a24519ae079fb89fcc41f73b
Miguel Angel Toledo Santiago <geaglts@gmail.com>
2020-10-30 17:58:13 -0600

    Aproximacion de soluciones, ejemplo con busqueda de raiz cuadrada

---
+++ aproximacion_soluciones.py
@@ -0,0 +1,20 @@
+objetivo = int(input('Ingresa un numero: '))
+# Que tan preciso queremos que sea
+# Basicamente epsilon es la precison que queramos que tenga nuestro programa
+# Que tan cerca queremos llegar a la respuesta
+epsilon = 0.01
+# **2 eleva un numero al cuadrado
+# Paso representa que tanto nos vamos a acercar en cada iteracion
+paso = epsilon**2
+respuesta = 0.0
+
+# Iteracion
+# abs(valor) regresa un valor absoluto
+while abs(respuesta**2 - objetivo) >= epsilon and respuesta <= objetivo:
+    print(abs(respuesta**2 - objetivo), respuesta)
+    respuesta += paso
+
+if abs(respuesta**2 - objetivo) >= epsilon:
+    print(f'No se encontro la raiz cuadrada del {objetivo}')
+else:
+    print(f'La raiz cuadrada de {objetivo} es {respuesta}')

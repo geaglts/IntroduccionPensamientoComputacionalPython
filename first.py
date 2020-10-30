af5710347d0047587bcf248c8324b8fc0fe081ee
Miguel Angel Toledo Santiago <geaglts@gmail.com>
2020-10-30 15:16:18 -0600

    Initial commit

---
+++ first.py
@@ -0,0 +1,28 @@
+# Error de sintaxis
+# 3 4
+
+# Error de semantica estatica
+# 4 - 'maria'
+
+# Esta expresion es valida
+# print(4 * ' maria')
+
+# Obtener el tipo de un valor, "Aun no se para que sirve"
+# print(type(3))
+# print(type('Hola'))
+# print(type(3.0))
+# print(type(True))
+
+# Concatenaciones
+# Cadenas de formato "Es como el template literals de js"
+# cadena_formato = f'{"hip " * 3}hurra'
+# print(cadena_formato)
+# nombre = "miguel"
+# print(f'{nombre} Bienvenido a mi programa')
+
+# Logitud de una cadena
+# print(len("Hola"))
+# Indexacion de una cadena
+# print("hola"[1])
+# Separacion de una parte
+# print("hola mundo"[0:4])

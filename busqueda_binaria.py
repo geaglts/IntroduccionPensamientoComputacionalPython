e72b98c6346827eeb14a79da1eaa7501b70375b7
Miguel Angel Toledo Santiago <geaglts@gmail.com>
2020-10-30 18:18:03 -0600

    Busqueda binaria ejemplo con la busqueda de raiz cuadrada

---
+++ busqueda_binaria.py
@@ -0,0 +1,18 @@
+objetivo = int(input("Ingresa un numero: "))
+epsilon = 0.0000001
+bajo = 0.0
+alto = max(1.0, objetivo)
+respuesta = (alto + bajo) / 2
+
+while abs(respuesta**2 - objetivo) >= epsilon:
+    # print(
+    #     f'abs={abs(respuesta**2 - objetivo)}, alto={alto}, bajo={bajo}, respuesta={respuesta}'
+    # )
+    if respuesta**2 < objetivo:
+        bajo = respuesta
+    else:
+        alto = respuesta
+
+    respuesta = (alto + bajo) / 2
+
+print(f'La raiz cuadrada de {objetivo} es {respuesta}')

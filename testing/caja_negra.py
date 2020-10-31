a57b77f0164bdbbf4003eaa1665c5255c3d54869
Miguel Angel Toledo Santiago <geaglts@gmail.com>
2020-10-30 20:53:08 -0600

    Diferentes tipos de testing

---
+++ testing/caja_negra.py
@@ -0,0 +1,31 @@
+import unittest
+
+# Con error
+# def suma(num_1, num_2):
+#     return abs(num_1) + num_2
+
+
+def suma(num_1, num_2):
+    return num_1 + num_2
+
+
+class CajaNegraTest(unittest.TestCase):
+    def test_suma_dos_positivos(self):
+        num_1 = 10
+        num_2 = 10
+
+        resultado = suma(num_1, num_2)
+
+        self.assertEqual(resultado, 20)
+
+    def test_suma_dos_negativos(self):
+        num_1 = -10
+        num_2 = -10
+
+        resultado = suma(num_1, num_2)
+
+        self.assertEqual(resultado, -20)
+
+
+if __name__ == '__main__':
+    unittest.main()

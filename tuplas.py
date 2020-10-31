cd5de80afc8f1c397181ec34d50e7dda0e0a050c
Miguel Angel Toledo Santiago <geaglts@gmail.com>
2020-10-30 20:03:47 -0600

    Tuplas

---
+++ tuplas.py
@@ -0,0 +1,31 @@
+def log(v):
+    print(v)
+
+
+my_tuple = ()
+log(type(my_tuple))
+my_tuple = (1, "dos", True)
+log(my_tuple[2])
+my_tuple = (1)
+log(type(my_tuple))
+my_tuple = (1, )
+log(type(my_tuple))
+
+# Suma de tuplas
+my_other_tuple = (2, 3, 4)
+my_tuple += my_other_tuple
+log(my_tuple)
+
+# Desempaquetar tuplas
+x, y, z = my_other_tuple
+log(f'x={x}, y={y}, z={z}')
+
+
+# Desempaquetar tuplas
+def coordenadas():
+    return (3, 4)
+
+
+coordenada = coordenadas()
+x, y = coordenada
+log(f'x={x}, y={y}')
